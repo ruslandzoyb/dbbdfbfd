24668eeac6847b7d2bdaaeafb37b2352b29b9a76
rusya <dnepr.ukrainec@gmail.com>
2020-01-02 21:07:00 +0200

    Добавьте файлы проекта.

---
+++ WebApplication3/models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.models
+{
+    public class ApplicationContext : IdentityDbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<user> Users { get; set; }
+
+    }
+}
